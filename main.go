cf2ae12ee620344865af5d833277b4ee8259d494
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-03-26 13:28:01 +0700

    add constant for session config

--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	sessionName       = "session_token"
+	expiryInSecond    = 120
+	isSessionSecure   = false
+	isSessionHttpOnly = false
+
+	baseUrl  = "localhost"
+	basePath = "/"
+	port     = 8000
+)
+
 var users = map[string]string{
 	"user1": "password1",
 	"user2": "password2",
@@ -35,60 +46,68 @@ func Signin(c *gin.Context) {
 	err := c.ShouldBindJSON(&cred)
 	if err != nil {
 		c.AbortWithStatus(http.StatusBadRequest)
+		return
 	}
 
 	expectedPassword, ok := users[cred.Username]
 	if !ok || expectedPassword != cred.Password {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	sessionToken := uuid.NewString()
-	expiredAt := time.Now().Add(120 * time.Second)
+	expiredAt := time.Now().Add(expiryInSecond * time.Second)
 
 	sessions[sessionToken] = Session{
 		username: cred.Username,
 		expiry:   expiredAt,
 	}
 
-	c.SetCookie("session_token", sessionToken, 120, "/", "localhost", false, false)
+	c.SetCookie(sessionName, sessionToken, expiryInSecond, basePath, baseUrl, isSessionSecure, isSessionHttpOnly)
 }
 
 func Welcome(c *gin.Context) {
-	sessionToken, err := c.Cookie("session_token")
+	sessionToken, err := c.Cookie(sessionName)
 	if err != nil {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 	userSession, exists := sessions[sessionToken]
 	fmt.Println(sessions)
 	if !exists {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	if userSession.isExpired() {
 		delete(sessions, sessionToken)
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	c.Writer.Write([]byte(fmt.Sprintf("Welcome, %s!", userSession.username)))
 }
 
 func Refresh(c *gin.Context) {
-	sessionToken, err := c.Cookie("session_token")
+	sessionToken, err := c.Cookie(sessionName)
 	if err != nil {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	userSession, exists := sessions[sessionToken]
 	if !exists {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	if userSession.isExpired() {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	newSessionToken := uuid.NewString()
-	expiredAt := time.Now().Add(120 * time.Second)
+	expiredAt := time.Now().Add(expiryInSecond * time.Second)
 
 	sessions[newSessionToken] = Session{
 		username: userSession.username,
@@ -96,17 +115,18 @@ func Refresh(c *gin.Context) {
 	}
 
 	delete(sessions, sessionToken)
-	c.SetCookie("session_token", newSessionToken, 120, "/", "localhost", false, false)
+	c.SetCookie(sessionName, newSessionToken, expiryInSecond, basePath, baseUrl, isSessionSecure, isSessionHttpOnly)
 }
 
 func Logout(c *gin.Context) {
-	sessionToken, err := c.Cookie("session_token")
+	sessionToken, err := c.Cookie(sessionName)
 	if err != nil {
 		c.AbortWithStatus(http.StatusUnauthorized)
+		return
 	}
 
 	delete(sessions, sessionToken)
-	c.SetCookie("session_token", "", 0, "/", "localhost", false, false)
+	c.SetCookie(sessionName, "", 0, basePath, baseUrl, isSessionSecure, isSessionHttpOnly)
 }
 
 func main() {
@@ -115,5 +135,5 @@ func main() {
 	router.GET("/welcome", Welcome)
 	router.POST("/refresh", Refresh)
 	router.POST("/logout", Logout)
-	router.Run("localhost:8000")
+	router.Run(fmt.Sprintf("%s:%v", baseUrl, port))
 }
